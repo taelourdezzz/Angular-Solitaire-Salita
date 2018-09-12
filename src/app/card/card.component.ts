529012857d339cf41fc7b816f5100437b09534f3
taelourdezzz <mastertaeman9152@yahoo.com>
2018-09-13 06:47:41 +0800

    With shuffle

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Card } from '../card';
 import { DECK } from '../card-data-set';
 import { NgStyle } from '@angular/common';
+import { ShuffleService } from '../shuffle.service';
 
 @Component({
   selector: 'app-card',
@@ -12,11 +13,17 @@ import { NgStyle } from '@angular/common';
 export class CardComponent implements OnInit {
   
   cards = DECK; 
-  
-  constructor() { }
+  shuffledCards;
+  constructor(private shuffleService: ShuffleService) { }
+
+  shuffleCards(cards){
+    this.shuffledCards = this.shuffleService.shuffle(cards);
+    return this.shuffledCards;
+  }
 
   ngOnInit() {
-
+    this.shuffledCards = this.shuffleCards(this.cards);
+    console.log(this.shuffledCards);
   }
 
   onCreate(card: Card) {
